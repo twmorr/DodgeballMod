72451d431721d461c9253d313ffc2318514d3a39
Taylor <taylormrris56@gmail.com>
2026-08-25 12:08:51 -0400

    add basic homing ability

--- Content/Projectiles/DodgeballProjectile.cs
+++ Content/Projectiles/DodgeballProjectile.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,8 +8,7 @@ namespace DodgeballMod.Content.Projectiles
 {
     public class DodgeballProjectile : ModProjectile
     {
-        public override string Texture =>
-            $"Terraria/Images/Projectile_{ProjectileID.BeachBall}";
+        public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.BeachBall}";
 
         public override void SetDefaults()
         {
@@ -23,6 +24,60 @@ namespace DodgeballMod.Content.Projectiles
 
             Projectile.tileCollide = true;
             Projectile.ignoreWater = false;
+        }
+
+        public override void AI()
+        {
+            NPC target = FindClosestEnemy(500f);
+
+            // No enemy nearby. Just continue moving normally.
+            if (target == null)
+                return;
+
+            // Get a vector pointing from the ball to the enemy.
+            Vector2 directionToTarget = target.Center - Projectile.Center;
+
+            // Reduce that vector to direction only.
+            directionToTarget.Normalize();
+
+            // This is the velocity we'd ideally like to have.
+            Vector2 desiredVelocity = directionToTarget * 12f;
+
+            // Gradually turn our current velocity toward it.
+            Projectile.velocity = Vector2.Lerp(Projectile.velocity, desiredVelocity, 0.08f);
+        }
+
+        private NPC FindClosestEnemy(float maxDistance)
+        {
+            // Start with no target selected.
+            NPC closestEnemy = null;
+
+            // Any enemy farther away than this will be ignored.
+            float closestDistance = maxDistance;
+
+            // Check every NPC slot in the world.
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+
+                // Skip NPCs that shouldn't be targeted by homing projectiles.
+                if (!npc.CanBeChasedBy())
+                    continue;
+
+                // Measure the distance from the dodgeball to this NPC.
+                float distance = Vector2.Distance(Projectile.Center, npc.Center);
+
+                // If this NPC is closer than our current best target,
+                // remember it as the new closest enemy.
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestEnemy = npc;
+                }
+            }
+
+            // Returns the closest valid enemy, or null if none were in range.
+            return closestEnemy;
         }
     }
 }
