a16e356dbb2318ce0abe212275873256e4c4e3db
Taylor <taylormrris56@gmail.com>
2026-08-25 10:22:22 -0400

    initial ranged dodgeball

---
+++ Content/Projectiles/DodgeballProjectile.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DodgeballMod.Content.Projectiles
+{
+    public class DodgeballProjectile : ModProjectile
+    {
+        public override string Texture =>
+            $"Terraria/Images/Projectile_{ProjectileID.BeachBall}";
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 20;
+
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.DamageType = DamageClass.Ranged;
+
+            Projectile.timeLeft = 300;
+
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = false;
+        }
+    }
+}
